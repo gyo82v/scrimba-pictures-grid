0eb151c4962f812d713dcbe9e334e99da5910725
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-08-12 22:46:29 +0200

    major update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-
+import {data} from "./data"
 
 function App() {
+  //tailwin styles
+  const container = `grid gap-[0.5em] grid-cols-[repeat(auto-fit,minmax(150px,1fr))] 
+                     grid-flow-row-dense auto-rows-[150px] m-6`
+  const article = `flex items-center justify-center text-[2em] shadow-lg shadow-neutral-800/30
+                   transform hover:scale-105`
+  const img = `w-full h-full object-cover rounded-md`
+  const wide = `col-span-2`
+  const tall = `row-span-2`
+  const big = `col-span-2 row-span-2`
 
+  const imgArray = data.map(d => (
+    <article className={article} key={d.id}>
+      <img 
+        src={d.img} 
+        alt={d.alt} 
+        className={`${img} ${d.wide ? wide : d.tall ? tall : d.big ? big : ""}`} 
+      />
+    </article>
+  ))
+
+  //
 
   return (
-   <div className="bg-red-600">
-      <h1>images here</h1>
-   </div>
+   <main className={container}>
+      {imgArray}
+   </main>
   )
 }
 
---
+++ src/data.js
@@ -0,0 +1,237 @@
+import autumn from "./images/autumn.jpg"
+import autumn1 from "./images/autumn1.jpg"
+import beach from "./images/beach.jpg"
+import beach1 from "./images/beach1.jpg"
+import castle from "./images/castle.jpg"
+import dawn from "./images/dawn.jpg"
+import dawn1 from "./images/dawn1.jpg"
+import field from "./images/field.jpg"
+import field1 from "./images/field1.jpg"
+import forest from "./images/forest.jpg"
+import lake from "./images/lake.jpg"
+import lake1 from "./images/lake1.jpg"
+import lake2 from "./images/lake2.jpg"
+import mountain from "./images/mountain.jpg"
+import mountain1 from "./images/mountain1.jpg"
+import mountain2 from "./images/mountain2.jpg"
+import mountain3 from "./images/mountain3.jpg"
+import mountain4 from "./images/mountain4.jpg"
+import sea from "./images/sea.jpg"
+import sea1 from "./images/sea1.jpg"
+import sea2 from "./images/sea2.jpg"
+import sea3 from "./images/sea3.jpg"
+import snow from "./images/snow.jpg"
+import snow1 from "./images/snow1.jpg"
+import tree from "./images/tree.jpg"
+import tree1 from "./images/tree1.jpg"
+
+export const data = [
+    {
+        id : 1, 
+        img : autumn, 
+        alt : "a trail in the forest in autumn.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 2, 
+        img : autumn1, 
+        alt : "a lake and a forest during autumn in the background.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 3, 
+        img : beach, 
+        alt : "a tropical beach and clear skies.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 4, 
+        img : beach1, 
+        alt : "a desert beach, during night and a green aurora in the sky.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 5, 
+        img : castle, 
+        alt : "a medieval castle in the middle of a forest.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 6, 
+        img : dawn, 
+        alt : "green hills submerged by the mist.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 7, 
+        img : dawn1, 
+        alt : "a lonely house on the tp of an hill surrounded by mist.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 8, 
+        img : field, 
+        alt : "a rice field surrounded by trees.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 9, 
+        img : field1, 
+        alt : "a deserted field duting winter at sunrise.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 10, 
+        img : forest, 
+        alt : "a forest with tall green trees in the morning.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 11, 
+        img : lake, 
+        alt : "an empty bench facing a quiet lake in the winter.",
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 12, 
+        img : lake1, 
+        alt : "shore of a huge lake , at sunset and a pair of empty canoe.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 13, 
+        img : lake2, 
+        alt : "a lonely church in a small island in the middle of a lake.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 14, 
+        img : mountain, 
+        alt : "mount fuji with snow on the top.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 15, 
+        img : mountain1, 
+        alt : "a field with purple flower with snowy mountains in the background.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 16, 
+        img : mountain2, 
+        alt : "rolling green hills surrounded by mist.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 17, 
+        img : mountain3, 
+        alt : "a big rock on the shore of a lake in winter.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 18, 
+        img : mountain4, 
+        alt : "a pack of dears with mountains in the background.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 19, 
+        img : sea, 
+        alt : "calm sen at night with clear sky and stars.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 20, 
+        img : sea1, 
+        alt : "an house on the shore of a calm lake.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 21, 
+        img : sea2, 
+        alt : "a lonely tree in the middle of the sea with purple water and skies.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 22, 
+        img : sea3, 
+        alt : "a sea with mountains in the distance and cloudy skies.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 23, 
+        img : snow, 
+        alt : "a tree on the top of a hill during winter.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 24, 
+        img : snow1, 
+        alt : "a snowy deserted field.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 25, 
+        img : tree, 
+        alt : "a tree with purple leaves on the shore of a lake.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+    {
+        id : 26, 
+        img : tree1, 
+        alt : "four chairs and a table under a tall tree at dawn.", 
+        wide : false, 
+        tall : false, 
+        big: false
+    },
+]
